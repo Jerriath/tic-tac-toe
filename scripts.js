5f08d2553868b81f3c1489ebaee35e6e20fa341a
Jerry Zhang <jerryzhang101@yahoo.com>
2021-06-16 13:14:21 -0400

    Created function to check game status

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ let gameBoard = (function(){
     
 
     function initBoard() {
-        let cols = board.length;
-        let rows = board[0].length;
+        let cols = board[0].length;
+        let rows = board.length;
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
                 board[i][j].square = _makeSquare(); //makes squares
@@ -19,8 +19,8 @@ let gameBoard = (function(){
 
     //Function for rendering the board data
     function render() {
-        let cols = board.length;
-        let rows = board[0].length;
+        let cols = board[0].length;
+        let rows = board.length;
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
                 _fillSquare(board[i][j].square, i, j);
@@ -56,7 +56,6 @@ let gameBoard = (function(){
     function selectSquare(i, j)  {
         let value = "x";
         _chooseSquare(value, i, j);
-        render();
     }
 
     //Function to add markers on the board
@@ -64,6 +63,131 @@ let gameBoard = (function(){
         if (board[i][j].value == null)
         {
             board[i][j].value = value;
+            render();
+            let status = checkGame();
+
+        }
+    }
+
+    //Function to check if game is over; returns 1=playerWin, 2=compWin, 3=draw 0=notOver
+    function checkGame() {
+        let cols = board[0].length;
+        let rows = board.length;
+        //Check columns
+        for (let j = 0; j < cols; j++)
+        {
+            let value = board[0][j].value;
+            for (let i = 1; i < rows; i++)
+            {
+                if (board[i][j].value != value)
+                {
+                    break;
+                }
+                if (i = 2)
+                {
+                    if (value == "x")
+                    {
+                        return 1;
+                    }
+                    if (value == "o")
+                    {
+                        return 2;
+                    }
+                }
+            }
+        }
+        //Check rows
+        for (let i = 0; i < rows; i++)
+        {
+            let value = board[i][0].value;
+            for (let j = 1; j < cols; j++)
+            {
+                if (board[i][j].value != value)
+                {
+                    break;
+                }
+                if (j = 2)
+                {
+                    if (value == "x")
+                    {
+                        return 1;
+                    }
+                    if (value == "o")
+                    {
+                        return 2;
+                    }
+                }
+            }
+        }
+        //Check diagonals
+        let value = board[0][0].value;
+        if (value == board[1][1].value && value == board[2][2])
+        {
+            if (value == "x")
+            {
+                return 1;
+            }
+            else if (value == "o")
+            {
+                return 2;
+            }
+        }
+        value = board[0][2].value;
+        if (value == board[1][1].value && value == board[2][0])
+        {
+            if (value == "x")
+            {
+                return 1;
+            }
+            else if (value == "o")
+            {
+                return 2;
+            }
+        }
+        //Check draw
+        let over = checkFull();
+        if (over)
+        {
+            return 3;
+        }
+        return 0;
+    }
+
+    //Function to check if board is full
+    function checkFull() {
+        let cols = board[0].length;
+        let rows = board.length;
+        for (let i = 0; i < rows; i++)
+        {
+            for (let j = 0; j < cols; j++)
+            {
+                if (board[i][j] == null)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //Function to add a message if game is over
+    function displayEnd(status)
+    {
+        if (status == 0)
+        {
+            return;
+        }
+        if (status == 1)
+        {
+            
+        }
+        if (status == 2)
+        {
+            
+        }
+        if (status == 3)
+        {
+            
         }
     }
 
@@ -96,7 +220,7 @@ let game = (function() {
     let playerScore = document.querySelector("#playerScore");
     let compScore = document.querySelector("#compScore");
     let clearBtn = document.querySelector("#clear");
-    
+
 });
 
 
